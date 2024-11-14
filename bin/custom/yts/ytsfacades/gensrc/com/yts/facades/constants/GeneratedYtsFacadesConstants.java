6e828792b4f007e0e63460277c82e6e2135137bb
daniel <danieldiniz1@icloud.com>
2024-11-14 12:45:28 -0300

    Set Up a Git Repository

---
+++ bin/custom/yts/ytsfacades/gensrc/com/yts/facades/constants/GeneratedYtsFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 de nov. de 2024 11:47:27                 ---
+ * ----------------------------------------------------------------
+ */
+package com.yts.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYtsFacadesConstants
+{
+	public static final String EXTENSIONNAME = "ytsfacades";
+	
+	protected GeneratedYtsFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bin/custom/yts/ytsinitialdata/gensrc/com/yts/initialdata/constants/GeneratedYtsInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 de nov. de 2024 11:47:27                 ---
+ * ----------------------------------------------------------------
+ */
+package com.yts.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYtsInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "ytsinitialdata";
+	
+	protected GeneratedYtsInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
