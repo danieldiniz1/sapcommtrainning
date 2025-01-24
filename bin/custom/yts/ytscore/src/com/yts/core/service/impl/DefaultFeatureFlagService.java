718a725079d731f0d95f75d4b7e2a45980f47c23
daniel <danieldiniz1@icloud.com>
2025-01-24 12:18:59 -0300

    #DD: feat - [SERVICE LAYER] adicionado serviço de busca se FF está ativada

--- bin/custom/yts/ytscore/src/com/yts/core/service/impl/DefaultFeatureFlagService.java
+++ bin/custom/yts/ytscore/src/com/yts/core/service/impl/DefaultFeatureFlagService.java
@@ -1,4 +1,20 @@
 package com.yts.core.service.impl;
 
-public class DefaultFeatureFlagService {
+import com.yts.core.dao.FeatureFlagDao;
+import com.yts.core.service.FeatureFlagService;
+import org.apache.log4j.Logger;
+
+public class DefaultFeatureFlagService implements FeatureFlagService {
+
+    private static final Logger LOG = Logger.getLogger(DefaultFeatureFlagService.class);
+    private FeatureFlagDao featureFlagDao;
+
+    @Override
+    public Boolean isFeatureEnabled(String featureCode) {
+        return featureFlagDao.isFeatureEnabled(featureCode);
+    }
+
+    public void setFeatureFlagDao(FeatureFlagDao featureFlagDao) {
+        this.featureFlagDao = featureFlagDao;
+    }
 }
